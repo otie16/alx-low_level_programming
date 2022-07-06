5bbda1acd18c32c2284bb41ae1c90f405ed55eb1
otie16 <biggieestone@gmail.com>
2022-07-06 13:26:43 -0700

    Hot fixwq

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,33 +5,17 @@
  */
 void jack_bauer(void)
 {
-int i, j;
-for (i = 0; i < 24; i++)
+int hour, min;
+for (hour = 0; hour < 24; hour++)
 {
-for (j = 0; j < 60; j++)
+for (min = 0; min < 60; min++)
 {
-if (i < 10)
-{
-_putchar('0');
-_putchar(i + '0');
-}
-else if (i >= 10)
-{
-_putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
-}
-if (j < 10)
-{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
 _putchar(':');
-_putchar('0');
-_putchar(j + '0');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+_putchar('\n');
 }
-else if (j > 10)
-{
-_putchar(':');
-_putchar((j / 10) + '0');
-_putchar((j % 10) + '0');
-}
-_putchar('\n'); 
 }
 }
