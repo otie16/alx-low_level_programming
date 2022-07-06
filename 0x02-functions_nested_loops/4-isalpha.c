87fc3ff0b476d9de4f7ee42ef48ba9f6942e0924
otie16 <biggieestone@gmail.com>
2022-07-06 11:08:21 -0700

    new c updates

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for upper and lower case
+ * @c: a char to be checked
+ * Return: returns 1 or 0
+ *
+ */
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+
+return (0);
