6952ed43b0dd514766aded38646ef72ebd0f77ad
otie16 <biggieestone@gmail.com>
2022-07-06 11:55:43 -0700

    new c updates

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute value of a number
+ * @c: parameter
+ * Return: absolute value of a number
+ */
+int _abs(int c)
+{
+if (c > 0 || c == 0)
+{
+return (c);
+}
+else
+return (c * -1);
+
