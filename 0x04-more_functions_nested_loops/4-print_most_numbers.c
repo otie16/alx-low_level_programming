5d85afc09d5014df130f3191e44421382bbfe22d
otie16 <biggieestone@gmail.com>
2022-07-08 14:07:09 -0700

    bug fixes

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,13 @@
  */
 void print_most_numbers(void)
 {
-char c = '0';
-while (c <= '9')
+int x;
+for (x = 0; x < 10; x++)
 {
-if (c != '2' && c != '4')
+if (x != 2 && x != 4)
 {
-_putchar(c);
-}
-c++;
+_putchar(x + '0');
 }
 _putchar('\n');
+}
 }
