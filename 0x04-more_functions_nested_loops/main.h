c2ed055d33557503fa873a2ca8a6546a37df7368
otie16 <biggieestone@gmail.com>
2022-07-08 14:58:19 -0700

    bug fixes

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -8,4 +9,6 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_square(int size);
+
 #endif 
