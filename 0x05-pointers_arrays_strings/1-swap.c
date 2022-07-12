0648736e2f41e755bc659a5e2b434ecb4b692823
otie16 <biggieestone@gmail.com>
2022-07-12 14:01:32 -0700

    hot fixes

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap two numbers
+ * @a: param 1
+ * @b: param 2
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+a = 7, b = 8;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+
+}
