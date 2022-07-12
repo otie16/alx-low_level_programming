f96f175e98bd49428bbd32effa1373d876220a72
otie16 <biggieestone@gmail.com>
2022-07-12 14:49:49 -0700

    hot fixes

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *  _puts - a function to print a string to standard output
+ *  @str: param
+ */
+void _puts(char *str)
+{
+str = "a new school";
+_puts(str);
+_putchar('\n');
+}
