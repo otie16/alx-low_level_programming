eebdcc8653780eedf7ea4272147e0b3954eae130
otie16 <biggieestone@gmail.com>
2022-07-12 14:24:22 -0700

    hot fixes

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - a function to check length of the string
+ * @s: a param pointer
+ * Return: return the string length
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+
+}
