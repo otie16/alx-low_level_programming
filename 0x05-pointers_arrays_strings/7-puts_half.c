113664bb403b68e17fb60817de4a8e43c175f962
Otobong Godwin <66527770+otie16@users.noreply.github.com>
2022-07-12 23:02:54 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,19 +14,19 @@ void puts_half(char *str)
 	}
 	if (c % 2 == 0)
 	{
-		for ((a = c / 2); str[a] != 0; a++)
-		{
-			l = str[a];
-			_putchar(l);
-		}
+	for ((a = c / 2); str[a] != 0; a++)
+	{
+	l = str[a];
+	_putchar(l);
+	}
 	}
 	else
-        {
-                for ((a = (c - 1) / 2); str[a] != 0; a++)
-                {
-                        l = str[a];
-                        _putchar(l);
-                }
+	{
+	for ((a = (c - 1) / 2); str[a] != 0; a++)
+         {
+	l = str[a];
+        _putchar(l);
+        }
         }
 	_putchar('\n');
 }
