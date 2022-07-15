9395f25769ebfda2e041b0a15d52ff8dc620d8a0
otie16 <biggieestone@gmail.com>
2022-07-15 12:14:31 -0700

    changes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,14 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strncat - a function that concats two strings
  * @dest: param
  * @src: a param
+ * @n: a param
  * Return: returns a value
  */
 char *_strncat(char *dest, char *src, int n)
 {
-
+	strncat(dest, src, n);
+	return  (dest);
 }
