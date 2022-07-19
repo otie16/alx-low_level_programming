b485a50f657f5c012289f7c7ad1d1d049771e944
otie16 <biggieestone@gmail.com>
2022-07-19 10:23:23 -0700

    added new c files

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - a function that copies memory area
+ * @dest: a param
+ * @src: a second param
+ * @n: a third param
+ * Return: return a value
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int a;
+
+for (a = 0; a < n; a++)
+{
+	dest[a] = src[a];
+	return (dest);
+}
