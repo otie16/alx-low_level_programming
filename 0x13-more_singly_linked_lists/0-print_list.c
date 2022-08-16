65299d02dfffe76e6270f3697c89c6140c9f684a
otie16 <biggieestone@gmail.com>
2022-08-16 15:09:52 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: list to print
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int counter;
+	while (h != NULL)
+	{
+		printf(" %d ", h->data);
+		h = h->next;
+		counter++;
+	}
+return (counter);
+}
